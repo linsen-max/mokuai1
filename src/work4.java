a437daef3410b4fcd499db72c6ff8801be8b89ca
linsen <lin19950507>
2022-03-03 23:31:31 +0800

    修改work4

--- src/work4.java
+++ src/work4.java
@@ -4,27 +4,24 @@ public class work4 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
-        int r =rd.nextInt(100);
-
+        System.out.println("请输入班级人数");
         int num = sc.nextInt();
         int[] arr = new int[num];
 
         int count = 0;
         int sum = 0;
-        for(int i = 0; i<num; i++) {
+        for(int i = 0; i< arr.length; i++) {
+            int r =rd.nextInt(101);
             arr[i] = r;
-            System.out.println("随机数"+r);
-            if (arr[i] <60);
+            if (arr[i] <60)
                 count++;
         }
-        for (int j = 0; j<num;j++) {
-            sum = arr[j];
-            sum += sum;
-
+        for (int j = 0; j<arr.length;j++) {
+            sum += arr[j];
         }
         System.out.println("和为"+sum);
         System.out.println("不及格人数为"+count);
-        System.out.println("和为"+sum);
+        System.out.println("平均分"+sum/num);
     }
 
 }
